1d5ef406f24b28eecd3b1de62703baeedda0c1ae
woogieOnAndOn <123wodnr@naver.com>
2022-04-10 13:42:45 +0900

    [환경] spring web project

---
+++ src/test/java/com/woogie/oauth/BeSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woogie.oauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
