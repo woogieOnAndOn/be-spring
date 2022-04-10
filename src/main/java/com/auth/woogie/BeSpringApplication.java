e935165ac8a9ec32bb333d89b8c7e99d21c4821b
woogieOnAndOn <123wodnr@naver.com>
2022-04-10 16:48:10 +0900

    [인증] 로그인과 에러처리

--- src/main/java/com/auth/woogie/BeSpringApplication.java
+++ src/main/java/com/auth/woogie/BeSpringApplication.java
@@ -1,10 +1,46 @@
 package com.auth.woogie;
 
+import java.util.Collections;
+import java.util.Map;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class BeSpringApplication {
+@RestController
+public class BeSpringApplication extends WebSecurityConfigurerAdapter {
+	@GetMapping("/user")
+	public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
+		return Collections.singletonMap("name", principal.getName());
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		// @formatter:off
+		System.out.println("configure");
+		http
+			.authorizeRequests(a -> a
+				.antMatchers("/", "/error", "/webjars/**").permitAll()
+				.anyRequest().authenticated()
+			)
+			.exceptionHandling(e -> e
+				.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+			)
+			.logout(l -> l
+				.logoutSuccessUrl("/").permitAll()
+			)
+			.oauth2Login();
+			
+		// @formatter:on
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BeSpringApplication.class, args);
